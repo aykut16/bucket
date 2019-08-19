bf073578a06fcfcf7c74a70b5024e36ee99ece8c
Maya <aikout.souleiman@gmail.com>
2019-08-19 23:33:19 +0300

    first commit

---
+++ src/main/java/com/zerotoproduction/bucketlist/BucketlistApplication.java
@@ -0,0 +1,16 @@
+package com.zerotoproduction.bucketlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+public class BucketlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BucketlistApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/zerotoproduction/bucketlist/DatabaseConfig.java
@@ -0,0 +1,25 @@
+package com.zerotoproduction.bucketlist;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+@Configuration
+public class DatabaseConfig {
+
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(dbUrl);
+        return new HikariDataSource(config);
+    }
+}
